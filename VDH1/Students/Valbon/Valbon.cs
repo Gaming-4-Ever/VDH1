98fbaf6eb86415a5ea3536ed8edb7819b5b433ef
valbon osmani <valb0043@hotmail.com>
2021-06-03 09:00:32 +0200

    Valbon - pull request

---
+++ VDH1/Students/Valbon/Valbon.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VDH1.Students.Valbon
+{
+    class Valbon
+    {
+        public string FullName { get; set; }
+        public Valbon(string fullName)
+        {
+            FullName = fullName;
+        }
+
+        Valbon person = new Valbon("Valbon Osmani");
+    }
+}
